39c58abad20311cdb3c9a77b84686c9d07ed2071
Andrii Pasarar <andrii.pasarar@chisw.com>
2024-04-14 13:24:33 +0300

    Nav dest changes

---
+++ app/src/main/java/com/example/mvvmsample/navigation/destinations/NavDestinations.kt
@@ -0,0 +1,27 @@
+package com.example.mvvmsample.navigation.destinations
+
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+
+sealed interface NavDestinations {
+    val route: String
+
+    data object BookDetails : NavDestinations {
+        const val BOOK_ID = "book_id"
+        override val route: String = "${Books.route}/{$BOOK_ID}"
+        val arguments = listOf(navArgument(BOOK_ID) { type = NavType.StringType })
+    }
+
+    data object Home : NavDestinations {
+        override val route: String = "home"
+    }
+
+    data object Favorite : NavDestinations {
+        override val route: String = "favorite"
+    }
+
+    data object Books : NavDestinations {
+        override val route: String = "book"
+        const val ARGUMENT_KEY = "KEY"
+    }
+}
