3a978db864383d4f077fb3b65e42b65b893fe78d
Andrii Pasarar <andrii.pasarar@chisw.com>
2024-06-18 17:44:07 +0300

    Added and implemented splash screen api

--- app/src/main/java/com/example/mvvmsample/MainActivity.kt
+++ app/src/main/java/com/example/mvvmsample/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.mvvmsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.mvvmsample.ui.app.App
 import com.example.mvvmsample.ui.theme.ComposeEducationTheme
 import com.example.mvvmsample.utils.LocalSemantics
@@ -12,9 +14,16 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: ExampleMainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isSplashShouldBeShown.value
+            }
+        }
         setContent {
             CompositionLocalProvider(LocalSemantics provides SemanticsStrings()) {
                 ComposeEducationTheme {
