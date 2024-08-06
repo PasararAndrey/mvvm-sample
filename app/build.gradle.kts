c15367a11819997686761cfab566625768c33315
Andrii Pasarar <andrii.pasarar@chisw.com>
2024-08-06 13:41:09 +0300

    Version 3 aab

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.findyourbook"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
